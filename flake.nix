29a37413e0a2f632cf404f700df07b0a0d2c0b35
Kenton Waters <kenton@boundlesswaters.com>
2026-03-11 19:59:01 -0500

    flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Learning HTML, CSS, TypeScript, React, Svelte, and more!";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShellNoCC {
+        buildInputs = with pkgs; [
+          google-chrome
+        ];
+
+        shellHook = ''
+          
+        '';
+      };
+    };
+}
